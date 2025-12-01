1b7ba2ba289d1befdca25e9baac19e324bdf7741
spawcy <mikhsannuryaman@gmail.com>
2025-12-01 14:31:12 +0700

    feat-ref!: add product list to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import ProductCard from "@/components/layouts/products/product-card";
 import Image from "next/image";
 
 const HEADER_CARD_ITEMS = [
@@ -19,18 +20,20 @@ export default function Home() {
   return (
     <main className="bg-background">
       <section id="header" className="bg-secondary py-20">
-        <div className="max-w-7xl items-center grid grid-cols-[1.5fr_1fr] gap-10 mx-auto">
+        <div className="max-w-7xl items-center grid md:grid-cols-[1.5fr_1fr] gap-10 mx-auto p-4">
           <div className="left">
-            <h2 className="text-7xl text-foreground">Smart IoT Gateways</h2>
+            <h2 className="text-5xl md:text-7xl text-foreground leading-normal md:leading-relaxed">
+              Smart IoT Gateways
+            </h2>
             <p className="mt-2.5 text-secondary-foreground">
               Bridging the Gap between Devices and the Cloud.
             </p>
 
-            <div className="grid grid-cols-2 gap-4 mt-10 max-w-xl">
+            <div className="grid md:grid-cols-2 gap-4 mt-10 max-w-xl">
               <HeaderCard />
             </div>
           </div>
-          <figure className="w-[35rem] ml-auto flex items-center justify-center">
+          <figure className="w-[20rem] md:w-[35rem] md:ml-auto flex items-center justify-center mx-auto">
             <Image
               src="/header__thumbnail.png"
               alt="header_thumbnail_image"
@@ -39,6 +42,13 @@ export default function Home() {
               className="block max-w-full"
             />
           </figure>
+        </div>
+      </section>
+
+      <section className="my-10 max-w-7xl mx-auto px-4">
+        <h2 className="text-2xl">New Product Arrival</h2>
+        <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-10 mt-10">
+          <ProductCard qty={10} />
         </div>
       </section>
     </main>
@@ -50,7 +60,7 @@ function HeaderCard() {
     <>
       {HEADER_CARD_ITEMS.map(({ title, text }, index) => (
         <div key={index}>
-          <b className="text-2xl font-bold">{title}</b>
+          <b className="text-xl md:text-2xl font-bold">{title}</b>
           <p className="mt-1.5">{text}</p>
         </div>
       ))}
