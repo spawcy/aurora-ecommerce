c3c9d5fdc1d6420e94897e603dfbcb2ad35b39d2
spawcy <mikhsannuryaman@gmail.com>
2025-12-02 15:03:35 +0700

    feat(api): add product API endpoint for handling product request

---
+++ src/app/api/product/route.ts
@@ -0,0 +1,19 @@
+import { PRODUCTS_PLACEHOLDER } from "@/app/const/static/product-placeholder";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+  const data_from_db = PRODUCTS_PLACEHOLDER;
+  const product_id = req.nextUrl.searchParams.get("product_id");
+
+  const find_product_by_id = data_from_db.find((data) => {
+    return data.product_id === product_id;
+  });
+
+  console.log(find_product_by_id);
+
+  if (!product_id) {
+    return NextResponse.json({ status: 404, data: null, message: "product not found" });
+  }
+
+  return NextResponse.json({ status: 200, data_temp: find_product_by_id, id_param: product_id, message: "fetching success" });
+}
