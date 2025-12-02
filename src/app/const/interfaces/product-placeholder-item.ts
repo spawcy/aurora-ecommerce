afd4b41a5208ad5a41690ca100e012bd297817c7
spawcy <mikhsannuryaman@gmail.com>
2025-12-02 13:54:46 +0700

    chore(types): add interface for product placeholder data

---
+++ src/app/const/interfaces/product-placeholder-item.ts
@@ -0,0 +1,8 @@
+export interface ProductPlaceholderItem {
+  product_id: string;
+  product_title: string;
+  product_image: string;
+  product_available: boolean;
+  product_description: string;
+  product_categories: string[];
+}
