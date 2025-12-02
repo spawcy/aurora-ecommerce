4e91b7abe1a653cec547de316978b6ea86b97117
spawcy <mikhsannuryaman@gmail.com>
2025-12-02 14:31:48 +0700

    feat(navbar-!ref)): separate nav item to isolate cleanup component

--- src/components/containers/navbar.tsx
+++ src/components/containers/navbar.tsx
@@ -1,9 +1,8 @@
 "use client";
 
-import { NAV_ITEMS } from "@/data/static/nav-items";
 import { HeartIcon } from "lucide-react";
-import Link from "next/link";
 import { useState } from "react";
+import NavItem from "@/components/layouts/navbar/nav-item";
 export default function Navbar() {
   const [hamActive, setHamActive] = useState<boolean>(false);
   const menuTransitionClass = hamActive && "active";
@@ -11,52 +10,28 @@ export default function Navbar() {
   return (
     <header className="fixed max-sm:bottom-0 sm:top-0 left-0 right-0 py-4 px-4 max-sm:border-t sm:border-b border-border bg-primary-foreground z-10">
       <ul
-        className={`navHeight bg-primary-foreground w-full rounded-md px-4 space-y-5
+        className={`navHeight max-sm:flex sm:hidden bg-primary-foreground w-full rounded-md px-4 space-y-5 flex-col
       transition duration-300 ${menuTransitionClass}`}
       >
-        <NavItems />
+        <NavItem />
       </ul>
 
       <div className="max-w-7xl mx-auto flex items-center justify-between">
         <p className="text-xl">Aurora</p>
         <div className="right flex max-sm:gap-4 sm:gap-6 items-center">
           <nav className="max-sm:hidden sm:flex max-w-sm gap-6 gap-mx-auto flex items-center justify-between">
-            <NavItems />
+            <NavItem />
           </nav>
           <figure className="max-sm:block sm:hidden size-14 cursor-pointer">
-            <svg
-              className={`ham hamRotate ham8 block max-w-full ${
-                hamActive && "active"
-              }`}
-              viewBox="0 0 100 100"
-              onClick={() => setHamActive((prev) => !prev)}
-            >
-              <path
-                className="line top"
-                d="m 30,33 h 40 c 3.722839,0 7.5,3.126468 7.5,8.578427 0,5.451959 -2.727029,8.421573 -7.5,8.421573 h -20"
-              />
+            <svg className={`ham hamRotate ham8 block max-w-full ${hamActive && "active"}`} viewBox="0 0 100 100" onClick={() => setHamActive((prev) => !prev)}>
+              <path className="line top" d="m 30,33 h 40 c 3.722839,0 7.5,3.126468 7.5,8.578427 0,5.451959 -2.727029,8.421573 -7.5,8.421573 h -20" />
               <path className="line middle" d="m 30,50 h 40" />
-              <path
-                className="line bottom"
-                d="m 70,67 h -40 c 0,0 -7.5,-0.802118 -7.5,-8.365747 0,-7.563629 7.5,-8.634253 7.5,-8.634253 h 20"
-              />
+              <path className="line bottom" d="m 70,67 h -40 c 0,0 -7.5,-0.802118 -7.5,-8.365747 0,-7.563629 7.5,-8.634253 7.5,-8.634253 h 20" />
             </svg>
           </figure>
           <HeartIcon className="block max-w-full size-8 sm:size-6" />
         </div>
       </div>
     </header>
-  );
-}
-
-function NavItems() {
-  return (
-    <>
-      {NAV_ITEMS.map(({ name, href }, index) => (
-        <Link href={href} key={index} className="block">
-          {name}
-        </Link>
-      ))}
-    </>
   );
 }
