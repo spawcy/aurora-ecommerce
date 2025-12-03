1c78b19ebeffa606bc2e6877846a8c0f1a7bf799
spawcy <mikhsannuryaman@gmail.com>
2025-12-03 16:17:24 +0700

    ref:(cleanup) remove spacing prettiere plugin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import ProductCard from "@/components/layouts/products/product-card";
+import ProductLists from "@/components/layouts/products/product-lists";
 import Image from "next/image";
 
 const HEADER_CARD_ITEMS = [
@@ -18,34 +18,22 @@ export default function Home() {
       <section id="header" className="bg-secondary py-20">
         <div className="max-w-7xl items-center grid md:grid-cols-[1.5fr_1fr] gap-10 mx-auto p-4">
           <div className="left">
-            <h2 className="text-5xl md:text-7xl text-foreground leading-normal">
-              Smart IoT Gateways
-            </h2>
-            <p className="mt-2.5 text-secondary-foreground">
-              Bridging the Gap between Devices and the Cloud.
-            </p>
+            <h2 className="text-5xl md:text-7xl text-foreground leading-normal">Smart IoT Gateways</h2>
+            <p className="mt-2.5 text-secondary-foreground">Bridging the Gap between Devices and the Cloud.</p>
 
             <div className="grid md:grid-cols-2 gap-4 mt-10 max-w-xl">
               <HeaderCard />
             </div>
           </div>
           <figure className="w-[20rem] md:w-[35rem] md:ml-auto flex items-center justify-center mx-auto">
-            <Image
-              src="/header__thumbnail.png"
-              alt="header_thumbnail_image"
-              width={1080}
-              height={1080}
-              className="block max-w-full"
-            />
+            <Image src="/header__thumbnail.png" alt="header_thumbnail_image" width={1080} height={1080} className="block max-w-full" />
           </figure>
         </div>
       </section>
 
       <section className="my-10 max-w-7xl mx-auto px-4">
         <h2 className="text-2xl">New Product Arrival</h2>
-        <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-10 mt-10">
-          <ProductCard qty={10} />
-        </div>
+        <ProductLists className="md:grid-cols-3 lg:grid-cols-4" />
       </section>
     </main>
   );
