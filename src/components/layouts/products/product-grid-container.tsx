0500cd00d2e98e35f9ff0920121c686ea0e7e8c0
spawcy <mikhsannuryaman@gmail.com>
2025-12-03 16:16:50 +0700

    feat: implement loading state on products page when product is fetched

---
+++ src/components/layouts/products/product-grid-container.tsx
@@ -0,0 +1,23 @@
+"use client";
+import DynamicPagination from "@/components/common/dynamic-pagination";
+import ProductLists from "./product-lists";
+import SearchInput from "@/components/common/search-input";
+import { useState } from "react";
+import { useFetching } from "@/hooks/use-fetching";
+import { ProductCardLoading, ProductsNotFound } from "./product-card";
+
+export default function ProductGridContainer() {
+  const [search, setSearch] = useState("");
+  const { data: products, isLoading, error } = useFetching(`/api/products`);
+  const filteredProducts = products?.data ? products.data.filter((product: { product_title: string }) => product.product_title.toLowerCase().includes(search.toLowerCase())) : [];
+
+  return (
+    <section className="w-full px-4">
+      <SearchInput search={search} setSearch={setSearch} />
+      {isLoading && <ProductCardLoading />}
+      {error && <ProductsNotFound />}
+      <ProductLists data={filteredProducts} className="sm:grid-cols-2 lg:grid-cols-3" />
+      <DynamicPagination />
+    </section>
+  );
+}
