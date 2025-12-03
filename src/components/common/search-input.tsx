be86e0027c3d066560e9d60611b9400293bd46f2
spawcy <mikhsannuryaman@gmail.com>
2025-12-03 16:17:07 +0700

    feat(!REF): implement debounce on search input

---
+++ src/components/common/search-input.tsx
@@ -0,0 +1,26 @@
+import { InputGroup, InputGroupAddon, InputGroupInput } from "@/components/ui/input-group";
+import { useDebounce } from "@/hooks/use-debounce";
+import { SearchIcon } from "lucide-react";
+import { useEffect, useState } from "react";
+
+export default function SearchInput({ search, setSearch }: any) {
+  const [inputValue, setInputValue] = useState(search);
+  useEffect(() => {
+    setInputValue(search);
+  }, [search]);
+  const debouncedSearchTerm = useDebounce(inputValue, 500);
+  useEffect(() => {
+    if (debouncedSearchTerm !== search) {
+      setSearch(debouncedSearchTerm);
+    }
+  }, [debouncedSearchTerm]);
+
+  return (
+    <InputGroup className="max-w-sm ml-auto py-4">
+      <InputGroupInput value={inputValue} onChange={(e) => setInputValue(e.target.value)} placeholder="Search..." />
+      <InputGroupAddon>
+        <SearchIcon />
+      </InputGroupAddon>
+    </InputGroup>
+  );
+}
