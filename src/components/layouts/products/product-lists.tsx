1e5ac1c6f1aafb7767357262c5ce44d333a29412
spawcy <mikhsannuryaman@gmail.com>
2025-12-03 16:16:24 +0700

    feat: adjustment on props product render-list-item (add className)

--- src/components/layouts/products/product-lists.tsx
+++ src/components/layouts/products/product-lists.tsx
@@ -1,10 +1,20 @@
 "use client";
+import { useFetching } from "@/hooks/use-fetching";
 import ProductCard from "./product-card";
 
-export default function ProductLists() {
+export default function ProductLists({ data, className }: { data: any; className?: string }) {
+  const { data: products, isLoading, error } = useFetching("/api/products");
+
+  if (!products) return <p>There is no data found</p>;
+  if (isLoading) return <p>Loading products...</p>;
+  if (error) return <p>{error}</p>;
+
   return (
-    <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-4 mt-8">
-      <ProductCard />
+    // <div className={`grid sm:grid-cols-2 lg:grid-cols-3 gap-4 mt-8 ${className}`}>
+    <div className={`grid gap-4 mt-8 ${className}`}>
+      {products.data.map((product, index) => (
+        <ProductCard key={index} title={product.product_title} />
+      ))}
     </div>
   );
 }
