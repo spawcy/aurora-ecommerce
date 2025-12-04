36dce499c250755afa8e3c14a1c524736eae8a53
spawcy <mikhsannuryaman@gmail.com>
2025-12-04 11:21:28 +0700

    feat: seperate header-card-items into /static/data

---
+++ src/const/static/header-card-items.ts
@@ -0,0 +1,10 @@
+export const HEADER_CARD_ITEMS = [
+  {
+    title: "100+",
+    text: "Proven Automation Solutions",
+  },
+  {
+    title: "TRUSTED BY",
+    text: "Major Automotive Manufacturers",
+  },
+];
