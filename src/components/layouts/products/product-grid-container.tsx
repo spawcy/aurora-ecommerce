04d8722fbe40d17eb49328d7f978c12caf31f4bd
spawcy <mikhsannuryaman@gmail.com>
2025-12-04 11:20:05 +0700

    feat: implement search product on products page

--- src/components/layouts/products/product-grid-container.tsx
+++ src/components/layouts/products/product-grid-container.tsx
@@ -7,17 +7,27 @@ import { useFetching } from "@/hooks/use-fetching";
 import { ProductCardLoading, ProductsNotFound } from "./product-card";
 
 export default function ProductGridContainer() {
-  const [search, setSearch] = useState("");
+  const [search, setSearch] = useState<string>("");
   const { data: products, isLoading, error } = useFetching(`/api/products`);
-  const filteredProducts = products?.data ? products.data.filter((product: { product_title: string }) => product.product_title.toLowerCase().includes(search.toLowerCase())) : [];
+
+  const rawProducts = products?.data || [];
+
+  const productDataForList = search ? rawProducts.filter((product) => product.product_title.toLowerCase().includes(search.toLowerCase())) : rawProducts;
+  const productListData = { data: productDataForList };
+  const isDataNotFound = !isLoading && !error && productDataForList.length === 0;
 
   return (
     <section className="w-full px-4">
       <SearchInput search={search} setSearch={setSearch} />
       {isLoading && <ProductCardLoading />}
       {error && <ProductsNotFound />}
-      <ProductLists className="sm:grid-cols-2 lg:grid-cols-3" />
-      <DynamicPagination />
+      {isDataNotFound && <ProductsNotFound />}
+      {!isLoading && !error && productDataForList.length > 0 && (
+        <>
+          <ProductLists data={productListData} className="sm:grid-cols-2 lg:grid-cols-3" />
+          <DynamicPagination />
+        </>
+      )}
     </section>
   );
 }
