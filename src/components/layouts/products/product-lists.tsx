f6dc5bdd80be0b0db51798484eb5e8023796a41d
spawcy <mikhsannuryaman@gmail.com>
2025-12-08 15:04:57 +0700

    ref(products): Add quantity control for product list display

--- src/components/layouts/products/product-lists.tsx
+++ src/components/layouts/products/product-lists.tsx
@@ -2,11 +2,13 @@
 import { ProductPlaceholderItem } from "@/const/interfaces/product-placeholder-item";
 import ProductCard from "./product-card";
 
-export default function ProductLists({ data, className }: { data?: any; className?: string }) {
+export default function ProductLists({ count, data, className }: { count?: number; data: any; className?: string }) {
+  const rawData = data?.data || [];
+  const slicedData = count ? rawData.slice(0, count) : rawData;
   return (
     <div className={`grid gap-4 mt-8 ${className}`}>
-      {data?.data.map((product: ProductPlaceholderItem, index: number) => (
-        <ProductCard key={index} title={product.product_title} />
+      {slicedData.map((product: ProductPlaceholderItem, index: number) => (
+        <ProductCard key={index} title={product.product_title} image={product.product_image} href={product.product_id} />
       ))}
     </div>
   );
