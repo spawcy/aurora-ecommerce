e43c63a919168a1547546a283e2a63e7ac21ac44
spawcy <mikhsannuryaman@gmail.com>
2025-12-08 15:04:22 +0700

    ref:(cleanup) remove unused import, spacing, and unused code

--- src/components/layouts/products/product-card.tsx
+++ src/components/layouts/products/product-card.tsx
@@ -1,19 +1,16 @@
+import { ProductCardProps } from "@/const/interfaces/product-card-props";
 import Image from "next/image";
 import Link from "next/link";
 
-interface ProductCardProps {
-  title?: string;
-}
-
-export default function ProductCard({ title }: ProductCardProps) {
+export default function ProductCard({ title, image, href }: ProductCardProps) {
   return (
     <>
-      <Link href="/products/product-test" className="flex flex-col items-center justify-center border border-border rounded-md p-4 transition duration-300 hover:border-chart-1 group overflow-hidden">
-        <figure className="transition duration-300 group-hover:scale-85 group-hover:opacity-50 group-hover:-translate-y-2">
-          <Image src="/product__placeholder.png" alt="product__placeholder" width={250} height={250} className="block max-w-full" />
+      <Link href={`/products/${href}`} className="flex flex-col items-center justify-center border border-border rounded-md p-4 transition duration-300 hover:border-chart-1 group overflow-hidden">
+        <figure className="block transition duration-300 group-hover:scale-85 group-hover:opacity-50 group-hover:-translate-y-2">
+          <Image src={`${image ? image : "/product__placeholder.png"}`} alt="product__placeholder" width={250} height={250} className="block max-w-full aspect-square" />
         </figure>
         <div className="mt-6">
-          <p className="text-xl text-secondary-foreground transition-transform duration-300 translate-y-0 group-hover:-translate-y-4">{title || "2019 New Release Premium Chalk Reel"}</p>
+          <p className="text-xl text-secondary-foreground transition-transform duration-300 translate-y-0 group-hover:-translate-y-4">{title || "Default title"}</p>
           {/* <Button className="w-full transition-transform duration-300 translate-y-24 group-hover:-translate-y-0 py-7">Get A Quote</Button> */}
         </div>
       </Link>
