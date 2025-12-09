76109117f25b053e0b33163d120f01e500b577b0
spawcy <mikhsannuryaman@gmail.com>
2025-12-09 09:09:48 +0700

    fix: adjustment padding inline for mobile at product-not-found component

--- src/components/layouts/products/product-lists.tsx
+++ src/components/layouts/products/product-lists.tsx
@@ -17,13 +17,13 @@ export default function ProductLists({ count, data, className }: { count?: numbe
 
 export function ProductNotFound() {
   return (
-    <div className="h-screen flex flex-col items-center justify-center">
+    <div className="h-screen flex flex-col items-center justify-center px-4 sm:p-0">
       <div className="text-center max-w-md 2xl:max-w-2xl mx-auto">
         <figure className="block">
           <FrownIcon className="block size-14 2xl:size-24 mx-auto text-muted-foreground" />
         </figure>
         <p className="mt-6 text-2xl 2xl:text-6xl font-medium">Product Not Found</p>
-        <p className="text-muted-foreground mt-1 2xl:text-xl 2xl:mt-3.5  leading-relaxed">he product you are looking for might have been moved, sold out, or the link you followed is incorrect.</p>
+        <p className="text-muted-foreground mt-1 2xl:text-xl 2xl:mt-3.5  leading-relaxed">The product you are looking for might have been moved, sold out, or the link you followed is incorrect.</p>
       </div>
     </div>
   );
