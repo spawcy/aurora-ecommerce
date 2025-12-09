0dc7b1653f0fb6e44fd247b303cb5cf5c144e7cb
spawcy <mikhsannuryaman@gmail.com>
2025-12-09 09:05:33 +0700

    style: implement clean modern UI for not found state for product detail

--- src/components/layouts/products/product-lists.tsx
+++ src/components/layouts/products/product-lists.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { ProductPlaceholderItem } from "@/const/interfaces/product-placeholder-item";
 import ProductCard from "./product-card";
+import { FrownIcon } from "lucide-react";
 
 export default function ProductLists({ count, data, className }: { count?: number; data: any; className?: string }) {
   const rawData = data?.data || [];
@@ -10,6 +11,20 @@ export default function ProductLists({ count, data, className }: { count?: numbe
       {slicedData.map((product: ProductPlaceholderItem, index: number) => (
         <ProductCard key={index} title={product.product_title} image={product.product_image} href={product.product_id} />
       ))}
+    </div>
+  );
+}
+
+export function ProductNotFound() {
+  return (
+    <div className="h-screen flex flex-col items-center justify-center">
+      <div className="text-center max-w-md 2xl:max-w-2xl mx-auto">
+        <figure className="block">
+          <FrownIcon className="block size-14 2xl:size-24 mx-auto text-muted-foreground" />
+        </figure>
+        <p className="mt-6 text-2xl 2xl:text-6xl font-medium">Product Not Found</p>
+        <p className="text-muted-foreground mt-1 2xl:text-xl 2xl:mt-3.5  leading-relaxed">he product you are looking for might have been moved, sold out, or the link you followed is incorrect.</p>
+      </div>
     </div>
   );
 }
