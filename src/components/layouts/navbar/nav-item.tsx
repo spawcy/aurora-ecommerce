444595093d9face567c823e2f8db261e9c92b458
spawcy <mikhsannuryaman@gmail.com>
2025-12-09 12:15:18 +0700

    style: adjusting font-weight on navbar

--- src/components/layouts/navbar/nav-item.tsx
+++ src/components/layouts/navbar/nav-item.tsx
@@ -4,7 +4,7 @@ export default function NavItem() {
   return (
     <>
       {NAV_ITEMS.map(({ name, href }, index) => (
-        <Link href={href} key={index} className="hover:text-primary/50">
+        <Link href={href} key={index} className="text-primary hover:text-primary/50 2xl:text-lg font-medium">
           {name}
         </Link>
       ))}
