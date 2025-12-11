ce13a153ba05927d3368f3c2bb4df6a258762766
spawcy <mikhsannuryaman@gmail.com>
2025-12-11 15:49:54 +0700

    feat: add mailto redirect with toast promise wrapper

--- src/components/layouts/product-detail/product-actions.tsx
+++ src/components/layouts/product-detail/product-actions.tsx
@@ -6,7 +6,24 @@ import { toast } from "sonner";
 export default function ProductActions() {
   return (
     <div className="w-full my-6 py-6 border-t border-b border-border grid grid-cols-[5.20fr_.9fr] place-items-center gap-4">
-      <Button className="w-full py-7" onClick={() => toast.info("this feat will implement later!")}>
+      <Button
+        className="w-full py-7"
+        onClick={() => {
+          toast.promise(
+            new Promise((resolve) => {
+              setTimeout(() => {
+                window.location.href = "mailto:admin@example.com?subject=Request Pembelian Produk IoT&body=Halo Admin, Saya ingin melakukan pembelian produk IoT.%0AMohon informasi lebih lanjut terkait ketersediaan dan harga. Terima kasih.";
+                resolve(true);
+              }, 300);
+            }),
+            {
+              loading: "Opening email…",
+              success: "Email app opened!",
+              error: "Failed to open email.",
+            }
+          );
+        }}
+      >
         Get a quote
       </Button>
       <ProductLoveAction />
