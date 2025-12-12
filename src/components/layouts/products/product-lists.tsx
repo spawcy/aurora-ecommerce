6ff87ec7932d024c8f09aca1739601ab7f808103
spawcy <mikhsannuryaman@gmail.com>
2025-12-12 11:26:35 +0700

    refactor(imports): Clean up imports for ProductNotFound component

--- src/components/layouts/products/product-lists.tsx
+++ src/components/layouts/products/product-lists.tsx
@@ -1,7 +1,8 @@
 "use client";
 import { ProductPlaceholderItem } from "@/const/interfaces/product-placeholder-item";
 import ProductCard from "./product-card";
-import { FrownIcon } from "lucide-react";
+import NotFoundActions from "../not-found/not-found-actions";
+import NotFoundTitles from "../not-found/not-found-titles";
 
 export default function ProductLists({ count, data, className }: { count?: number; data: any; className?: string }) {
   const rawData = data?.data || [];
@@ -18,13 +19,8 @@ export default function ProductLists({ count, data, className }: { count?: numbe
 export function ProductNotFound() {
   return (
     <div className="h-screen flex flex-col items-center justify-center px-4 sm:p-0">
-      <div className="text-center max-w-md 2xl:max-w-2xl mx-auto">
-        <figure className="block">
-          <FrownIcon className="block size-14 2xl:size-24 mx-auto text-muted-foreground" />
-        </figure>
-        <p className="mt-6 text-2xl 2xl:text-6xl font-medium">Product Not Found</p>
-        <p className="text-muted-foreground mt-1 2xl:text-xl 2xl:mt-3.5  leading-relaxed">The product you are looking for might have been moved, sold out, or the link you followed is incorrect.</p>
-      </div>
+      <NotFoundTitles title="Product Not Found" text="The product you are looking for might have been moved, sold out, or the link you followed is incorrect" />
+      <NotFoundActions />
     </div>
   );
 }
