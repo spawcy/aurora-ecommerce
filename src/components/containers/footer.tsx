46d242f6c8519ef10e668ff883ccabb1b2c63ea1
spawcy <mikhsannuryaman@gmail.com>
2025-12-15 09:12:09 +0700

    ref: Extract layout visibility logic into useLayoutVisibility hook

--- src/components/containers/footer.tsx
+++ src/components/containers/footer.tsx
@@ -2,35 +2,30 @@
 import { FOOTER_LINK_ITEMS } from "@/const/static/footer-data-items";
 import Image from "next/image";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
-import { NO_NAV_FOOTER_PATH_ITEMS, VALID_EXACT_ROUTES } from "@/const/static/disable-nav-footer-path-items";
+import { useLayoutVisibility } from "@/hooks/use-layout-visibility";
 
 export default function Footer() {
-  const currentPath = usePathname();
-  const isValidRoute = () => {
-    if (VALID_EXACT_ROUTES.includes(currentPath)) return true;
-    const validDynamicPatterns = [/^\/products\/[a-z0-9]+$/i];
-    return validDynamicPatterns.some((pattern) => pattern.test(currentPath));
-  };
-  const shouldHideNavbarFooter = NO_NAV_FOOTER_PATH_ITEMS.includes(currentPath) || !isValidRoute();
-  if (shouldHideNavbarFooter) return null;
-
+  const shouldHideLayout: boolean = useLayoutVisibility();
   return (
-    <footer className="py-10 bg-accent">
-      <div className="max-w-7xl mx-auto grid px-4 sm:grid-cols-2 gap-20">
-        <div className="gap-3">
-          <Image src="/aurora__logo.png" alt="logo__aurora--light" width={125} height={20} className="block max-w-full cursor-pointer" />
-          <p className="text-3xl text-primary my-4">admin.aurora@gmail.com</p>
-          <p className="text-secondary-foreground">Professional industrial IoT equipment and sensors for automation.</p>
-        </div>
-        <ul className="grid grid-cols-3 gap-6 max-w-xl ml-auto">
-          {FOOTER_LINK_ITEMS.map((item, index) => (
-            <Link href={`/${item}`} key={index} className="capitalize text-primary hover:underline hover:text-foreground/50">
-              {item}
-            </Link>
-          ))}
-        </ul>
-      </div>
-    </footer>
+    <>
+      {!shouldHideLayout && (
+        <footer className="py-10 bg-accent">
+          <div className="max-w-7xl mx-auto grid px-4 sm:grid-cols-2 gap-20">
+            <div className="gap-3">
+              <Image src="/aurora__logo.png" alt="logo__aurora--light" width={125} height={20} className="block max-w-full cursor-pointer" />
+              <p className="text-3xl text-primary my-4">admin.aurora@gmail.com</p>
+              <p className="text-secondary-foreground">Professional industrial IoT equipment and sensors for automation.</p>
+            </div>
+            <ul className="grid grid-cols-3 gap-6 max-w-xl ml-auto">
+              {FOOTER_LINK_ITEMS.map((item, index) => (
+                <Link href={`/${item}`} key={index} className="capitalize text-primary hover:underline hover:text-foreground/50">
+                  {item}
+                </Link>
+              ))}
+            </ul>
+          </div>
+        </footer>
+      )}
+    </>
   );
 }
--- src/components/containers/navbar.tsx
+++ src/components/containers/navbar.tsx
@@ -1,51 +1,21 @@
 "use client";
-
-import { HeartIcon } from "lucide-react";
-import {  useState } from "react";
-import NavItem from "@/components/layouts/navbar/nav-item";
-import { usePathname, useRouter } from "next/navigation";
-import Image from "next/image";
-import { NO_NAV_FOOTER_PATH_ITEMS, VALID_EXACT_ROUTES } from "@/const/static/disable-nav-footer-path-items";
+import { useState } from "react";
+import { useLayoutVisibility } from "@/hooks/use-layout-visibility";
+import NavMobile from "../layouts/navbar/nav-mobile";
+import NavDesktop from "../layouts/navbar/nav-desktop";
 
 export default function Navbar() {
-  const router = useRouter();
-  const currentPath = usePathname();
   const [hamActive, setHamActive] = useState<boolean>(false);
-  const menuTransitionClass = hamActive && "active";
-  const isValidRoute = () => {
-    if (VALID_EXACT_ROUTES.includes(currentPath)) return true;
-    const validDynamicPatterns = [/^\/products\/[a-z0-9]+$/i];
-    return validDynamicPatterns.some((pattern) => pattern.test(currentPath));
-  };
-  const shouldHideNavbarFooter = NO_NAV_FOOTER_PATH_ITEMS.includes(currentPath) || !isValidRoute();
-  if (shouldHideNavbarFooter) return null;
-  return (
-    <header className="fixed max-sm:bottom-0 sm:top-0 left-0 right-0 py-6 px-4 max-sm:border-t sm:border-b border-border bg-primary-foreground z-10">
-      <ul
-        className={`navHeight max-sm:flex sm:hidden bg-primary-foreground w-full rounded-md px-4 space-y-5 flex-col
-      transition duration-300 ${menuTransitionClass}`}
-      >
-        <NavItem />
-      </ul>
+  const shouldHideLayout: boolean = useLayoutVisibility();
 
-      <div className="max-w-7xl mx-auto flex items-center justify-between px-4">
-        <figure className="block h-full cursor-pointer">
-          <Image src="/aurora__logo.png" alt="logo__aurora--light" width={125} height={20} className="block max-w-full cursor-pointer" />
-        </figure>
-        <div className="right flex max-sm:gap-4 sm:gap-6 items-center">
-          <nav className="max-sm:hidden sm:flex max-w-sm gap-6 gap-mx-auto flex items-center justify-between">
-            <NavItem />
-          </nav>
-          <figure className="max-sm:block sm:hidden size-14 cursor-pointer">
-            <svg className={`ham hamRotate ham8 block max-w-full ${hamActive && "active"}`} viewBox="0 0 100 100" onClick={() => setHamActive((prev) => !prev)}>
-              <path className="line top" d="m 30,33 h 40 c 3.722839,0 7.5,3.126468 7.5,8.578427 0,5.451959 -2.727029,8.421573 -7.5,8.421573 h -20" />
-              <path className="line middle" d="m 30,50 h 40" />
-              <path className="line bottom" d="m 70,67 h -40 c 0,0 -7.5,-0.802118 -7.5,-8.365747 0,-7.563629 7.5,-8.634253 7.5,-8.634253 h 20" />
-            </svg>
-          </figure>
-          <HeartIcon onClick={() => router.push("/wishlist")} className="block max-w-full size-8 sm:size-6 cursor-pointer hover:text-primary/60" />
-        </div>
-      </div>
-    </header>
+  return (
+    <>
+      {!shouldHideLayout && (
+        <header className="fixed max-sm:bottom-0 sm:top-0 left-0 right-0 py-6 px-4 max-sm:border-t sm:border-b border-border bg-primary-foreground z-10">
+        <NavMobile hamActive={hamActive} />
+        <NavDesktop hamActive={hamActive} setHamActive={setHamActive} />
+        </header>
+      )}
+    </>
   );
 }
