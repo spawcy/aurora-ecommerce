abaf6a9d132dd2af86477b06832d50aa797c6869
spawcy <mikhsannuryaman@gmail.com>
2025-12-22 13:15:36 +0700

    static: add placeholder data for product gallery

---
+++ src/const/static/product-gallery-placeholder-items.ts
@@ -0,0 +1,17 @@
+export const PRODUCT_GALLERY_PLACEHOLDER_ITEMS = [
+  {
+    id: 1,
+    url: "/product__placeholder.png",
+    title: "A large body of water surrounded by mountains",
+  },
+  {
+    id: 2,
+    url: "/product__placeholder__2.png",
+    title: "Abstract streaks of pink and blue on black",
+  },
+  {
+    id: 3,
+    url: "/product__placeholder__3.png",
+    title: "Mountain Summit",
+  },
+];
