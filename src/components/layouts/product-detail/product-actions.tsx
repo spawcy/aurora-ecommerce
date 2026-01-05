dad8828c6109f97938dba82f058e3573a140a57a
ikhlasdansantai <initesting5@gmail.com>
2026-01-05 16:58:44 +0700

    refactor: move get a quote product logic to reusable hook

--- src/components/layouts/product-detail/product-actions.tsx
+++ src/components/layouts/product-detail/product-actions.tsx
@@ -1,29 +1,14 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import ProductLoveAction from "./product-love-action";
-import { toast } from "sonner";
+import { useProductMailto } from "@/hooks/use-product-mailto";
 
 export default function ProductActions({ title }: { title: string }) {
+  const { handlePurchaseRequest } = useProductMailto(title);
+
   return (
     <div className="w-full my-6 py-6 border-t border-b border-border grid grid-cols-[5.20fr_.9fr] place-items-center gap-4">
-      <Button
-        className="w-full py-7"
-        onClick={() => {
-          toast.promise(
-            new Promise((resolve) => {
-              setTimeout(() => {
-                window.location.href = `mailto:admin@example.com?subject=Request Pembelian Produk IoT&body=Halo Admin, Saya ingin melakukan pembelian produk ${title}.%0AMohon informasi lebih lanjut terkait ketersediaan dan harga. Terima kasih.`;
-                resolve(true);
-              }, 300);
-            }),
-            {
-              loading: "Opening email…",
-              success: "Email app opened!",
-              error: "Failed to open email.",
-            }
-          );
-        }}
-      >
+      <Button className="w-full py-7" onClick={handlePurchaseRequest}>
         Get a quote
       </Button>
       <ProductLoveAction />
