b8bb52a4178f2aaa2ac3971bf435003b6f33f27f
ikhlasdansantai <initesting5@gmail.com>
2026-01-05 16:58:32 +0700

    ref: remove unused import on dynamic-breadcrumb component

--- src/components/common/DynamicBreadcrumb.tsx
+++ src/components/common/DynamicBreadcrumb.tsx
@@ -1,6 +1,5 @@
 "use client";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
 import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "@/components/ui/breadcrumb";
 
 export default function DynamicBreadcrumb({ data }: { data: string[] }) {
